175d3a33206b82241312efaed81609fa160fb3b8
uloulou <ulou@ucsd.edu>
2024-05-07 00:02:46 -0700

    firework

--- fireworks/src/App.js
+++ fireworks/src/App.js
@@ -1,148 +1,152 @@
-import Particles from "react-particles";
-import { loadFull } from "tsparticles";
+import React, {useEffect, useState} from "react";
+import Particles, {initParticlesEngine} from "@tsparticles/react";
+import {loadFull} from "tsparticles";
+import logo from "./logo.svg";
 import "./App.css";
+import particlesOptions from "./particles.json";
+import { useCallback } from "react";
+
 
 export default function App() {
-  const particleInit = async (engine) => {
-    await loadFull(engine);
-  };
-
-  const particleConfig = {
-    fullScreen: {
-      enable: true
-    },
-    detectRetina: true,
-    background: {
-      color: "#000"
-    },
-    fpsLimit: 60,
-    emitters: {
-      // direction: ["left", 'right'],
-      life: {
-        count: 0,
-        duration: 0.1,
-        delay: 0.1
+    const particleInit = async (engine) => {
+      await loadFull(engine);
+    };
+  
+    const particleConfig = {
+      fullScreen: {
+        enable: true
       },
-      rate: {
-        delay: 0.01,
-        quantity: 1
+      detectRetina: true,
+      background: {
+        color: "#000"
       },
-      size: {
-        width: 100,
-        height: 0
-      },
-      position: {
-        y: 100,
-        x: 50
-      }
-    },
-    particles: {
-      number: {
-        value: 0
-      },
-      life: {
-        count: 1
-      },
-      shape: {
-        type: "line"
-      },
-      size: {
-        value: { min: 1, max: 100 },
-        animation: {
-          enable: true,
-          sync: true,
-          speed: 150,
-          destroy: "min"
+      fpsLimit: 60,
+      emitters: {
+        // direction: ["left", 'right'],
+        life: {
+          count: 0,
+          duration: 0.1,
+          delay: 0.1
+        },
+        rate: {
+          delay: 0.01,
+          quantity: 1
+        },
+        size: {
+          width: 100,
+          height: 0
+        },
+        position: {
+          y: 100,
+          x: 50
         }
       },
-      stroke: {
-        color: {
-          value: "#303030"
+      particles: {
+        number: {
+          value: 0
         },
-        width: 3
-      },
-      rotate: {
-        path: true
-      },
-      move: {
-        enable: true,
-        speed: { min: 10, max: 20 },
-        outModes: {
-          default: "destroy",
-          top: "none"
+        life: {
+          count: 1
         },
-        trail: {
-          fillColor: "#000",
-          enable: true,
-          length: 10
-        }
-      },
-      destroy: {
-        mode: "split",
-        split: {
-          count: 1,
-          factor: { value: 1 / 3 },
-          rate: {
-            value: 100
+        shape: {
+          type: "line"
+        },
+        size: {
+          value: { min: 1, max: 100 },
+          animation: {
+            enable: true,
+            sync: true,
+            speed: 150,
+            destroy: "min"
+          }
+        },
+        stroke: {
+          color: {
+            value: "#303030"
           },
-          particles: {
-            color: {
-              value: ["#FF0000", "#FFD700", "#00FF00", "#00BFFF", "#FF1493"]
+          width: 3
+        },
+        rotate: {
+          path: true
+        },
+        move: {
+          enable: true,
+          speed: { min: 10, max: 20 },
+          outModes: {
+            default: "destroy",
+            top: "none"
+          },
+          trail: {
+            fillColor: "#000",
+            enable: true,
+            length: 10
+          }
+        },
+        destroy: {
+          mode: "split",
+          split: {
+            count: 1,
+            factor: { value: 1 / 3 },
+            rate: {
+              value: 100
             },
-            stroke: {
-              width: 0
-            },
-            number: {
-              value: 0
-            },
-            collisions: {
-              enable: false
-            },
-            opacity: {
-              value: 1,
-              animation: {
-                enable: true,
-                speed: 0.6,
-                minimumValue: 0.1,
-                sync: false,
-                startValue: "max",
-                destroy: "min"
-              }
-            },
-            shape: {
-              type: "circle"
-            },
-            size: {
-              value: { min: 2, max: 3 },
-              animation: {
-                enable: false
-              }
-            },
-            life: {
-              count: 1,
-              duration: {
-                value: {
-                  min: 1,
-                  max: 2
-                }
-              }
-            },
-            move: {
-              enable: true,
-              gravity: {
+            particles: {
+              color: {
+                value: ["#FF0000", "#FFD700", "#00FF00", "#00BFFF", "#FF1493"]
+              },
+              stroke: {
+                width: 0
+              },
+              number: {
+                value: 0
+              },
+              collisions: {
                 enable: false
               },
-              speed: 2,
-              direction: "none",
-              random: true,
-              straight: false,
-              outMode: "destroy"
+              opacity: {
+                value: 1,
+                animation: {
+                  enable: true,
+                  speed: 0.6,
+                  minimumValue: 0.1,
+                  sync: false,
+                  startValue: "max",
+                  destroy: "min"
+                }
+              },
+              shape: {
+                type: "circle"
+              },
+              size: {
+                value: { min: 2, max: 3 },
+                animation: {
+                  enable: false
+                }
+              },
+              life: {
+                count: 1,
+                duration: {
+                  value: {
+                    min: 1,
+                    max: 2
+                  }
+                }
+              },
+              move: {
+                enable: true,
+                gravity: {
+                  enable: false
+                },
+                speed: 2,
+                direction: "none",
+                random: true,
+                straight: false,
+                outMode: "destroy"
+              }
             }
           }
         }
       }
-    }
-  };
-  return <Particles options={particleConfig} init={particleInit} />;
-}
-
+    };
+    return <Particles options={particleConfig} init={particleInit} />;
+  }
