75e6da7f6d152594cc2ce211fe07a70f5d3cf0f9
uloulou <ulou@ucsd.edu>
2024-05-07 11:49:24 -0700

    deploy

--- fireworks/src/App.js
+++ fireworks/src/App.js
@@ -1,145 +1,56 @@
-import "./styles.css";
-import Particles from "react-tsparticles";
-import { loadFull } from "tsparticles";
+import React, {useEffect, useState} from "react";
+import Particles, {initParticlesEngine} from "@tsparticles/react";
+import {loadFull} from "tsparticles";
+import logo from "./logo.svg";
+import "./App.css";
+import particlesOptions from "./particles.json";
 
-export default function App() {
-  const particlesInit = async (main) => {
-    console.log(main);
+function App() {
+    const [init, setInit] = useState(false);
 
-    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
-    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-    // starting from v2 you can add only the features you need reducing the bundle size
-    await loadFull(main);
-  };
-
-  return (
-    <div className="App">
-      <h1>Hello Coders!</h1>
-
-       <Particles
-      id="tsparticles"
-      init={particlesInit}
-
-      options={{
-        "fullScreen": {
-            "enable": true,
-            "zIndex": 1
-        },
-        "particles": {
-            "number": {
-                "value": 10,
-                "density": {
-                    "enable": false,
-                    "value_area": 800
-                }
-            },
-            "color": {
-                "value": "#fff"
-            },
-            "shape": {
-                "type": "star",
-                "options": {
-                    "sides": 5
-                }
-            },
-            "opacity": {
-                "value": 0.8,
-                "random": false,
-                "anim": {
-                    "enable": false,
-                    "speed": 1,
-                    "opacity_min": 0.1,
-                    "sync": false
-                }
-            },
-            "size": {
-                "value": 4,
-                "random": false,
-                "anim": {
-                    "enable": false,
-                    "speed": 40,
-                    "size_min": 0.1,
-                    "sync": false
-                }
-            },
-            "rotate": {
-                "value": 0,
-                "random": true,
-                "direction": "clockwise",
-                "animation": {
-                    "enable": true,
-                    "speed": 5,
-                    "sync": false
-                }
-            },
-            "line_linked": {
-                "enable": true,
-                "distance": 600,
-                "color": "#ffffff",
-                "opacity": 0.4,
-                "width": 2
-            },
-            "move": {
-                "enable": true,
-                "speed": 2,
-                "direction": "none",
-                "random": false,
-                "straight": false,
-                "out_mode": "out",
-                "attract": {
-                    "enable": false,
-                    "rotateX": 600,
-                    "rotateY": 1200
-                }
-            }
-        },
-        "interactivity": {
-            "events": {
-                "onhover": {
-                    "enable": true,
-                    "mode": ["grab"]
-                },
-                "onclick": {
-                    "enable": false,
-                    "mode": "bubble"
-                },
-                "resize": true
-            },
-            "modes": {
-                "grab": {
-                    "distance": 400,
-                    "line_linked": {
-                        "opacity": 1
-                    }
-                },
-                "bubble": {
-                    "distance": 400,
-                    "size": 40,
-                    "duration": 2,
-                    "opacity": 8,
-                    "speed": 3
-                },
-                "repulse": {
-                    "distance": 200
-                },
-                "push": {
-                    "particles_nb": 4
-                },
-                "remove": {
-                    "particles_nb": 2
-                }
-            }
-        },
-        "retina_detect": true,
-        "background": {
-            "color": "#111",
-            "image": "",
-            "position": "50% 50%",
-            "repeat": "no-repeat",
-            "size": "cover"
+    useEffect(() => {
+        if (init) {
+            return;
         }
-    }}
-    />
-    </div>
-  );
+
+        initParticlesEngine(async (engine) => {
+            await loadFull(engine);
+        }).then(() => {
+            setInit(true);
+        });
+    }, []);
+
+    return (
+        <div className="App">
+            {init && <Particles options={particlesOptions}/>}
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <p>
+                    Edit <code>src/particles.json</code> to customize Particles, then save
+                    to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+                <a
+                    className="App-link"
+                    href="https://particles.js.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    See Particles samples
+                </a>
+            </header>
+        </div>
+    );
 }
+
+export default App;
